5336b0c0c512cf181cb4cb94c47bf7de9685d4fb
Ignacio Heredia <iheredia@ifca.unican.es>
2024-06-10 11:56:13 +0200

    feat: check `FORCE_PULL` is true

--- download.sh
+++ download.sh
@@ -10,7 +10,7 @@ record_id="$RECORD_ID"
 check_command="df -h -x tmpfs -x devtmpfs | grep rshare"
 
 check_filesystem() {
-    eval "$check_command" > /dev/null 2>&1 
+    eval "$check_command" > /dev/null 2>&1
     return $?
 }
 
@@ -33,8 +33,8 @@ fi
 data_dir="/storage/ai4os-storage/zenodo/$record_id"
 
 
-if ! [ -z "$FORCEPULL" ]; then
-    echo "FORCEPULL set, deleting data dir"
+if [ "$FORCE_PULL" = true ] ; then
+    echo "FORCE_PULL set to true, deleting data dir"
     rm -rf $data_dir
 fi
 
