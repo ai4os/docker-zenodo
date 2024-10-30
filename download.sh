be9210a789e5d0237643d6cb3b78c783de8e1495
Saúl Fernández <sftobias@ifca.unican.es>
2024-10-30 13:25:00 +0100

    fix: dataset as url

--- download.sh
+++ download.sh
@@ -5,7 +5,18 @@
 # ------------------------------------------------------------------------
 # Download file based on RecordID from environment variable
 
+#As doi can be either a DOI or a URL, we need to normalize it to a path safe string
+
 doi="$DOI"
+doi=$1
+
+if [[ $doi == http* ]]; then
+    doi_path="$(echo "$doi" | sed -E 's|http(s)?://||; s|^www.||; s|/$|| ; s|/|-|g')" 
+else
+    doi_path="$doi"
+fi
+
+# echo "$doi_path"
 
 check_command="df -h -x tmpfs -x devtmpfs | grep rshare"
 
@@ -30,7 +41,7 @@ if [ "$(ls /storage 2>&1)" = "ls: reading directory '/storage': Input/output err
     exit 1
 fi
 
-data_dir="/storage/ai4os-storage/datasets/$doi"
+data_dir="/storage/ai4os-storage/datasets/$doi_path"
 
 
 if [ "$FORCE_PULL" = true ] ; then
